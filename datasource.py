df8e51d14815395fa3f08459e5a8e6f293e25e11
helenduz <62923883+helenduz@users.noreply.github.com>
2021-05-03 16:48:51 +0800

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -11,8 +11,15 @@ class DataSource:
         '''
         Note: if you choose to implement the constructor, this does *not* count as one of your implemented methods.
         '''
+    def connect(self):
+        try:
+            connection = psycopg2.connect(database="tut", user="tut", password="farm498lamp")
+        except Exception as e:
+            print("Connection error: ", e)
+            exit()
+        return connection
 
-    def keyword_search(connection):
+    def keyword_search(self, connection):
         try:
             cursor = connection.cursor()
             query = "SELECT product_name FROM products"
@@ -22,18 +29,11 @@ class DataSource:
             print ("Something went wrong when executing the query: ", e)
             return None
     
-def connect():
-    try:
-        connection = psycopg2.connect(database="tut", user="tut", password="farm498lamp")
-    except Exception as e:
-        print("Connection error: ", e)
-        exit()
-    return connection
 
 if __name__ == '__main__':
     # your code to test your function implementations goes here.
     test = DataSource()
-    connection = connect()
+    connection = test.connect()
     results = test.keyword_search(connection)
     if results is not None:
         print("Query results: ")
